39d20c258b28dfc828a7d155045f84c6673eeff1
Tom <tom.early@gmail.com>
2020-07-03 13:30:51 +0100

    Recreated App.vue and removed HelloWorld.vue example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import dataService from "@/api/dataService";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  mounted() {
+    this.loadPhotos();
+  },
+  methods: {
+    async loadPhotos() {
+      this.photos = await dataService.getPhotos(
+        "query=paris&orientation=landscape"
+      );
+    }
+  },
+  data() {
+    return {
+      photos: {}
+    };
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
