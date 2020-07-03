ae70cb7232741a885666788c46642b7d7a442c3b
Tom <tom.early@gmail.com>
2020-07-03 13:43:40 +0100

    Added Thumbnail.vue component and passed props from Gallery.vue

---
+++ src/components/Thumbnail.vue
@@ -0,0 +1,28 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	props: {
+    colour: {
+      type: String
+    },
+    altDescription: {
+      type: String
+    },
+    profileUrl: {
+      type: String
+    },
+    thumbUrl: {
+      type: String
+    },
+    thumbLinkUrl: {
+      type: String
+    },
+    firstName: {
+      type: String
+    }
+  },
+}
+</script>
