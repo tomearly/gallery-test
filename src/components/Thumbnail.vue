965833706c110bee4c068e2781b68c0794122de3
Tom <tom.early@gmail.com>
2020-07-03 13:53:26 +0100

    Added the thumbnail and thumbnail overlay and styled

--- src/components/Thumbnail.vue
+++ src/components/Thumbnail.vue
@@ -1,10 +1,27 @@
 <template>
-	<div></div>
+  <div class="thumbnail">
+    <a :href="thumbLinkUrl">
+      <div class="thumbnail-image" :style="style">
+        <img :alt="altDescription" :src="thumbUrl" />
+      </div>
+    </a>
+    <ThumbnailOverlay
+      class="thumbnail-overlay"
+      :first-name="firstName"
+      :profile-url="profileUrl"
+      :thumb-link-url="thumbLinkUrl"
+    >
+    </ThumbnailOverlay>
+  </div>
 </template>
 
 <script>
+import ThumbnailOverlay from "@/components/ThumbnailOverlay";
 export default {
-	props: {
+  components: {
+    ThumbnailOverlay
+  },
+  props: {
     colour: {
       type: String
     },
@@ -24,5 +41,43 @@ export default {
       type: String
     }
   },
-}
+  computed: {
+    style () {
+      return 'border: 2px solid ' + this.colour;
+    }
+  },
+};
 </script>
+
+<style lang="scss">
+.thumbnail {
+  position: relative;
+}
+
+.thumbnail-image {
+  overflow: hidden;
+  height: 133px;
+  width: 200px;
+}
+
+.thumbnail-image img {
+  transition: transform 0.5s ease; /* Animation */
+}
+
+.thumbnail-image:hover img {
+  transform: scale(1.05);
+  position: relative;
+}
+
+.thumbnail-overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(255, 255, 255, 0.75);
+  padding: 8px;
+}
+
+.thumbnail-overlay:hover {
+  background-color: rgba(255, 255, 255, 1);
+}
+</style>
