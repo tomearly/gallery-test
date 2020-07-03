9314a8c5cff9431556d385efae96d7513d152a33
Tom <tom.early@gmail.com>
2020-07-03 13:32:26 +0100

    Added gallery class and empty gallery file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div></div>
+  <gallery :photos="photos"> </gallery>
 </template>
 
 <script>
 import dataService from "@/api/dataService";
+import Gallery from "@/components/Gallery";
 
 export default {
   name: "App",
+  components: {
+    Gallery
+  },
   mounted() {
     this.loadPhotos();
   },
@@ -26,4 +30,11 @@ export default {
 </script>
 
 <style lang="scss">
+.gallery {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: grid;
+  grid-gap: 24px;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+}
 </style>
